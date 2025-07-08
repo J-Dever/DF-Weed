7a4232ac4ebc8db07b5d299e93eea24403a68db6
Jesper Dev <heningssonj24@gmail.com>
2025-07-08 16:09:41 +0200

    Add files via upload

---
+++ item.lua
@@ -0,0 +1,5 @@
+ --DF weed 
+    ['weed_seeds'] = { name = 'weed_seeds', label = 'Weed Seeds', weight = 100, type = 'item', image = 'weed_seed.png', unique = false, useable = true, description = 'Weed seeds for planting' },
+    ['weed'] = { name = 'weed', label = 'Weed', weight = 200, type = 'item', image = 'weed.png', unique = false, useable = true, description = 'Raw Weed' },
+    ['processed_weed'] = { name = 'processed_weed', label = 'Processed Weed', weight = 300, type = 'item', image = 'processed_weed.png', unique = false, useable = true, description = 'Processed Weed' },
+    ['joint'] = { name = 'joint', label = 'Joint', weight = 0, type = 'item', image = 'joint.png', unique = false, useable = true, shouldClose = true, description = 'Sidney would be very proud at you' },
